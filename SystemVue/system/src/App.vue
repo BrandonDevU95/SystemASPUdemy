a7d829cd6ed91846cb86754534bb3d1dcbdc76e8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2020-02-10 21:04:58 -0600

    Added project VueJS an Vuetify

---
+++ SystemVue/system/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <template>
+          <v-list-item :to="{name: 'home'}">
+            <v-list-item-action>
+              <v-icon>home</v-icon>
+            </v-list-item-action>
+            <v-list-item-title>
+              Inicio
+            </v-list-item-title>
+          </v-list-item>
+        </template>
+        <template>
+          <v-list-goup>
+            <v-list-title slot="activator">
+              
+            </v-list-title>
+          </v-list-goup>
+        </template>|
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="blue darken-3"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">SystemVue</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>      
+    </v-app-bar>
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-slide-y-transition mode="out-in">
+          <router-view/>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-footer dark height="auto">
+      <v-layout justify-center>
+        <v-flex text-md-center>
+          <v-card flat tile color="primary" class="white--text">
+            <v-card-text class="white--text" pt-0>  
+              BrandonVargas
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data: () => ({
+       drawer: null
+    }),
+  }
+</script>
